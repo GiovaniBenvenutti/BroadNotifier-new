e40435ed8f6ac8866c43cc0f9274bb14990ddc75
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2024-01-28 16:14:02 -0300

    o

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { environment } from "../environments/environment";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-
-
+import { environment } from '../environments/environment';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'BroadNotifier';
 
-message:any = null;
-constructor() {}
-ngOnInit(): void {
-  this.requestPermission();
-  //this.listen();
-}
-requestPermission() {
-  const messaging = getMessaging();
-  getToken(messaging, 
-   { vapidKey: environment.firebase.vapidKey}).then(
-     (currentToken) => {
-       if (currentToken) {
-         console.log("Hurraaa!!! we got the token.....");
-         console.log(currentToken);
-       } else {
-         console.log('No registration token available. Request permission to generate one.');
-       }    
-   }).catch((err) => {
-      console.log('An error occurred while retrieving token. ', err);   
-  });
-}
-/*
-listen() {
-  const messaging = getMessaging();
-  onMessage(messaging, (payload) => {
-    console.log('Message received. ', payload);
-    this.message=payload;
-  });
-}
-*/
+  message: any = null;
+  constructor() {}
+  ngOnInit(): void {
+    this.requestPermission();
+    this.listen();
+  }
+  requestPermission() {
+    const messaging = getMessaging();
+    getToken(messaging, { vapidKey: environment.firebase.vapidKey })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('Hurraaa!!! we got the token.....');
+          console.log(currentToken);
+        } else {
+          console.log(
+            'No registration token available. Request permission to generate one.'
+          );
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err);
+      });
+  }
+
+  listen() {
+    const messaging = getMessaging();
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      this.message = payload;
+    });
+  }
 }
